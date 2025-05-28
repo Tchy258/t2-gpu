e680f40132c44dd2ee6d702a1d1072693ed9c9f1
Tchy258 <tchy258@yahoo.cl>
2025-05-28 15:22:44 -0400

    Ahora si recibe los argumentos

--- src/opencl_benchmark.cpp
+++ src/opencl_benchmark.cpp
@@ -4,13 +4,12 @@
 
 int main(int argc, char** argv) {
     std::cout << "Test del juego de la vida con OpenCL" << std::endl;
-    //if (argc < 2) {
-    //    std::cerr << "Uso: " << argv[0] << " <iterations> [output.csv]" << std::endl;
-    //    return 1;
-    //}
+    if (argc < 2) {
+        std::cerr << "Uso: " << argv[0] << " <iterations> [output.csv]" << std::endl;
+        return 1;
+    }
     GameOfLife* game = new GameOfLifeOpenCL();
-    //int iterations = std::atoi(argv[1]);
-    int iterations = 3;
+    int iterations = std::atoi(argv[1]);
     benchmark(game, iterations, argc > 2 ? argv[2] : std::string());
     delete game;
     return 0;
