8ae4310e4c73d050618b296ad851996762cd3234
Vicentendistes <112288602+Vicentendistes@users.noreply.github.com>
2025-05-29 18:17:56 -0400

    Cambios Cuda

--- include/game_of_life_cuda.hpp
+++ include/game_of_life_cuda.hpp
@@ -1,5 +1,5 @@
-#ifndef GAME_OF_LIFE_CUDA_HPP
-#define GAME_OF_LIFE_CUDA_HPP
+#ifndef GAME_OF_LIFE_CUDA_H
+#define GAME_OF_LIFE_CUDA_H
 
 #include "game_of_life.hpp"
 #include "constants.h"            // define GRID_ROWS, GRID_COLS
@@ -15,7 +15,13 @@ public:
     void initializeRandom() override;
     void step() override;
 
-    std::vector<ubyte> getGrid() const;  // como en OpenCL: devuelve copia
+    unsigned char countAliveCells(unsigned int x0, unsigned int x1, unsigned int x2, 
+                                            unsigned int y0, unsigned int y1, unsigned int y2) {
+                                                throw std::logic_error("Counting should be done inside the kernel");
+                                            }
+    ARRAY_TYPE(unsigned char,) getGrid() const override {
+        throw std::logic_error("Not implemented for CUDA");
+    }
 
 private:
     int rows, cols;
@@ -24,11 +30,11 @@ private:
     std::vector<ubyte> h_grid, h_next;
     ubyte *d_grid = nullptr, *d_next = nullptr;
 
-    int threadsPerBlock = 256;
+    int threadsPerBlock = BLOCK_SIZE_X*BLOCK_SIZE_Y;
     int blocks;
 
     void allocDevice();
-    void freeDevice();
+    void freeDevice(); 
 };
 
 #endif
